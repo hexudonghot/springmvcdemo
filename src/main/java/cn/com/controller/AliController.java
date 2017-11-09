7038289cd79b5b800c0a6a8e85a2d71b61f294d1
hexudong <hexudong@sohu.com>
2017-11-09 10:06:44 +0800

    init

---
+++ src/main/java/cn/com/controller/AliController.java
@@ -0,0 +1,33 @@
+package cn.com.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/z")
+public class AliController
+{
+    private static Logger log = LoggerFactory.getLogger(AliController.class);
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "z")
+    public String pay() throws InterruptedException
+
+
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        log.info(df.format(new Date()));
+
+        //new Pict().pic();;
+        return "index";
+
+    }
+}
